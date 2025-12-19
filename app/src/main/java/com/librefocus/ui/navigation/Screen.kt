d9bdd2cd88ba20b05e96982b640df98c7a7d5230
Hammad Zafar Bawara <hammadbawara@gmail.com>
2025-12-19 16:28:48 +0500

    minor changes

---
+++ app/src/main/java/com/librefocus/ui/navigation/Screen.kt
@@ -0,0 +1,94 @@
+package com.librefocus.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Block
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Block
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.librefocus.R
+
+enum class Screen(
+    val route: String,
+    val label: String,
+    val selectedIcon: @Composable () -> Unit,
+    val unselectedIcon: @Composable () -> Unit
+) {
+    Home(
+        route = "home",
+        label = "Home",
+        selectedIcon = {
+            Icon(
+                imageVector = Icons.Filled.Home,
+                contentDescription = null
+            )
+        },
+        unselectedIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Home,
+                contentDescription = null
+            )
+        }
+    ),
+    Stats(
+        route = "stats",
+        label = "Stats",
+        selectedIcon = {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.ic_graph_filled),
+                contentDescription = null
+            )
+        },
+        unselectedIcon = {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.ic_graph_outlined),
+                contentDescription = null
+            )
+        }
+    ),
+
+    Limits (
+        route = "limits",
+        label = "Limits",
+        selectedIcon = {
+            Icon(
+                imageVector = Icons.Filled.Block,
+                contentDescription = null
+            )
+        },
+        unselectedIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Block,
+                contentDescription = null
+            )
+        }
+    ),
+
+    Settings (
+        route = "settings",
+        label = "Settings",
+        selectedIcon = {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = null
+            )
+        },
+        unselectedIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Settings,
+                contentDescription = null
+            )
+        }
+    );
+
+
+    companion object {
+        val entries: List<Screen> = Screen.entries
+    }
+}
