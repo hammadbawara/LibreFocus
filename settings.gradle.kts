1964feeb2c2d4201d5327ecb9384866c262dc4ee
mfaisaltech244 <137287107+mfaisaltech244@users.noreply.github.com>
2026-03-26 23:49:39 +0500

    start gamification

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.12.3" apply false
+        id("com.android.library") version "8.12.3" apply false
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
